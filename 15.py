8ac60ed6248364bf32076dcc44f5a602f63e2eb6
Narek Maloyan <maloyan@ispras.ru>
2018-11-02 03:11:13 +0300

    15

--- 15.py
+++ 15.py
@@ -1,16 +1,26 @@
-def f(cur_pos, arr):
-    print(cur_pos)
-    if cur_pos == 0:
-        return True
-    else:
-        b = False
-        for i in range(len(arr)):
-            if i - arr[i] == cur_pos or arr[i] + i == cur_pos:
-                b = b or f(i, arr)
-        return b
+from collections import deque
+
 a = eval(input())
-if f(-1, a):
+p = [0] * len(a)
+
+queue = deque([0])
+ans = False
+while queue:
+    cur_ind = queue.popleft()
+    if cur_ind == len(a) - 1:
+        ans = True
+        break
+    p[cur_ind] == 1
+    left = cur_ind - 1
+    right = cur_ind + a[cur_ind]
+    if right < len(a) and p[right] == 0:
+        queue.append(right)
+        p[right] = 1
+    if left > 0 and p[left] == 0:
+        queue.append(left)
+        p[left] = 1
+
+if ans:
     print("YES")
 else:
     print("NO")
- 
